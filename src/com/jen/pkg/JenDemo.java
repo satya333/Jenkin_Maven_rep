f98aff347e8cababd938c31b9909681975cb01ce
Satya <satya2tester@gmail.com>
2016-08-06 16:35:11 +0530

    Maven

---
+++ src/com/jen/pkg/JenDemo.java
@@ -0,0 +1,26 @@
+package com.jen.pkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class JenDemo {
+	
+	@Test
+	public void testA() throws InterruptedException
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\Satya\\Softwares\\Firefox\\geckodriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Satya\\Softwares\\chromedriver_win32\\chromedriver.exe");
+			
+		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new ChromeDriver();
+	
+		driver.get("https://www.google.co.in");
+		
+		Thread.sleep(10);
+		//driver.close();
+	}
+	
+
+}
