d3cb4f6967932ed693b6aba4b89e21d4fe653b33
Satya <satya2tester@gmail.com>
2016-08-13 16:03:05 +0530

    Assert

--- src/com/jen/pkg/JenDemo.java
+++ src/com/jen/pkg/JenDemo.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
+
 public class JenDemo {
 	
+	private static final boolean False = false;
+
 	@Test
 	public void testA() throws InterruptedException
 	{
@@ -17,7 +21,7 @@ public class JenDemo {
 		//WebDriver driver = new ChromeDriver();
 	
 		driver.get("https://www.google.co.in");
-		
+		Assert.assertTrue(False);;
 		Thread.sleep(10);
 		//driver.close();
 	}
