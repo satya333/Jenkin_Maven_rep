150848a58baabbde0519ceb44167b01199a42db3
Satya <satya2tester@gmail.com>
2016-08-13 18:49:57 +0530

    Report

--- src/com/jen/pkg/JenDemo.java
+++ src/com/jen/pkg/JenDemo.java
@@ -5,23 +5,44 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 import junit.framework.Assert;
 
 public class JenDemo {
-	
+	ExtentReports er = new ExtentReports("./ExtentReport.html");
 	@Test
 	public void testA() throws InterruptedException
 	{
 		System.setProperty("webdriver.gecko.driver", "C:\\Satya\\Softwares\\Firefox\\geckodriver.exe");
 		//System.setProperty("webdriver.chrome.driver", "C:\\Satya\\Softwares\\chromedriver_win32\\chromedriver.exe");
 			
+		
+		
+		ExtentTest t = er.startTest("testA");
+		t.log(LogStatus.PASS, "Hi--Bye");
+		er.endTest(t);
+		
+		er.flush();
 		WebDriver driver = new FirefoxDriver();
 		//WebDriver driver = new ChromeDriver();
-	
 		driver.get("https://www.google.co.in");
-		Assert.fail();
+		//Assert.fail();
 		Thread.sleep(10);
 		//driver.close();
+	}
+	
+	@Test
+	public void testB() throws InterruptedException
+	{
+		
+		ExtentTest t = er.startTest("testB");
+		t.log(LogStatus.FAIL, "Hi--Fail");
+		er.endTest(t);
+		er.flush();
+		
 	}
 	
 
