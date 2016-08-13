0c00d89ffcd90166ba9bc455a64d2b57f3d359a4
Satya <satya2tester@gmail.com>
2016-08-13 18:56:30 +0530

    sdfzf

--- src/com/jen/pkg/JenDemo.java
+++ src/com/jen/pkg/JenDemo.java
@@ -18,19 +18,15 @@ public class JenDemo {
 	{
 		System.setProperty("webdriver.gecko.driver", "C:\\Satya\\Softwares\\Firefox\\geckodriver.exe");
 		//System.setProperty("webdriver.chrome.driver", "C:\\Satya\\Softwares\\chromedriver_win32\\chromedriver.exe");
-			
-		
-		
 		ExtentTest t = er.startTest("testA");
 		t.log(LogStatus.PASS, "Hi--Bye");
 		er.endTest(t);
-		
-		er.flush();
-		WebDriver driver = new FirefoxDriver();
+		//er.flush();
+		//WebDriver driver = new FirefoxDriver();
 		//WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.co.in");
+		//driver.get("https://www.google.co.in");
 		//Assert.fail();
-		Thread.sleep(10);
+		//Thread.sleep(10);
 		//driver.close();
 	}
 	
